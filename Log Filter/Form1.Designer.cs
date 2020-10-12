0a56d1964e04d491c2b33a02ef409ba64522c608
Danny Bernal <xdbxdbx@users.noreply.github.com>
2020-10-12 17:15:49 -0400

    Initial project files commit

---
+++ Log Filter/Form1.Designer.cs
@@ -0,0 +1,219 @@
+﻿namespace Log_Filter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.PathTxtBox = new System.Windows.Forms.TextBox();
+            this.SelectFileBtn = new System.Windows.Forms.Button();
+            this.ReloadBtn = new System.Windows.Forms.Button();
+            this.LogBox = new System.Windows.Forms.ListBox();
+            this.FilterTextBox = new System.Windows.Forms.TextBox();
+            this.FilterBtn = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openOriginalFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveFilteredLogToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exportSelectionsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.LineLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.menuStrip1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // PathTxtBox
+            // 
+            this.PathTxtBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.PathTxtBox.Location = new System.Drawing.Point(12, 33);
+            this.PathTxtBox.Name = "PathTxtBox";
+            this.PathTxtBox.Size = new System.Drawing.Size(700, 20);
+            this.PathTxtBox.TabIndex = 0;
+            this.PathTxtBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.PathTxtBox_KeyDown);
+            // 
+            // SelectFileBtn
+            // 
+            this.SelectFileBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.SelectFileBtn.Location = new System.Drawing.Point(718, 31);
+            this.SelectFileBtn.Name = "SelectFileBtn";
+            this.SelectFileBtn.Size = new System.Drawing.Size(27, 23);
+            this.SelectFileBtn.TabIndex = 1;
+            this.SelectFileBtn.Text = "...";
+            this.SelectFileBtn.UseVisualStyleBackColor = true;
+            this.SelectFileBtn.Click += new System.EventHandler(this.SelectFileBtn_Click);
+            // 
+            // ReloadBtn
+            // 
+            this.ReloadBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.ReloadBtn.Location = new System.Drawing.Point(751, 31);
+            this.ReloadBtn.Name = "ReloadBtn";
+            this.ReloadBtn.Size = new System.Drawing.Size(75, 23);
+            this.ReloadBtn.TabIndex = 2;
+            this.ReloadBtn.Text = "Reload";
+            this.ReloadBtn.UseVisualStyleBackColor = true;
+            this.ReloadBtn.Click += new System.EventHandler(this.ReloadBtn_Click);
+            // 
+            // LogBox
+            // 
+            this.LogBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.LogBox.FormattingEnabled = true;
+            this.LogBox.HorizontalScrollbar = true;
+            this.LogBox.Location = new System.Drawing.Point(0, 90);
+            this.LogBox.Name = "LogBox";
+            this.LogBox.ScrollAlwaysVisible = true;
+            this.LogBox.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
+            this.LogBox.Size = new System.Drawing.Size(838, 511);
+            this.LogBox.TabIndex = 3;
+            this.LogBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.LogBox_KeyDown);
+            // 
+            // FilterTextBox
+            // 
+            this.FilterTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.FilterTextBox.BackColor = System.Drawing.SystemColors.Info;
+            this.FilterTextBox.Location = new System.Drawing.Point(12, 60);
+            this.FilterTextBox.Name = "FilterTextBox";
+            this.FilterTextBox.Size = new System.Drawing.Size(733, 20);
+            this.FilterTextBox.TabIndex = 4;
+            this.FilterTextBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.FilterTextBox_KeyDown);
+            // 
+            // FilterBtn
+            // 
+            this.FilterBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.FilterBtn.Location = new System.Drawing.Point(751, 58);
+            this.FilterBtn.Name = "FilterBtn";
+            this.FilterBtn.Size = new System.Drawing.Size(75, 23);
+            this.FilterBtn.TabIndex = 5;
+            this.FilterBtn.Text = "Filter";
+            this.FilterBtn.UseVisualStyleBackColor = true;
+            this.FilterBtn.Click += new System.EventHandler(this.FilterBtn_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(838, 24);
+            this.menuStrip1.TabIndex = 6;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openOriginalFileToolStripMenuItem,
+            this.saveFilteredLogToolStripMenuItem,
+            this.exportSelectionsToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(58, 20);
+            this.fileToolStripMenuItem.Text = "Utilities";
+            // 
+            // openOriginalFileToolStripMenuItem
+            // 
+            this.openOriginalFileToolStripMenuItem.Name = "openOriginalFileToolStripMenuItem";
+            this.openOriginalFileToolStripMenuItem.Size = new System.Drawing.Size(173, 22);
+            this.openOriginalFileToolStripMenuItem.Text = "Edit Log";
+            this.openOriginalFileToolStripMenuItem.Click += new System.EventHandler(this.openOriginalFileToolStripMenuItem_Click);
+            // 
+            // saveFilteredLogToolStripMenuItem
+            // 
+            this.saveFilteredLogToolStripMenuItem.Name = "saveFilteredLogToolStripMenuItem";
+            this.saveFilteredLogToolStripMenuItem.Size = new System.Drawing.Size(173, 22);
+            this.saveFilteredLogToolStripMenuItem.Text = "Export Filtered Log";
+            this.saveFilteredLogToolStripMenuItem.Click += new System.EventHandler(this.saveFilteredLogToolStripMenuItem_Click);
+            // 
+            // exportSelectionsToolStripMenuItem
+            // 
+            this.exportSelectionsToolStripMenuItem.Name = "exportSelectionsToolStripMenuItem";
+            this.exportSelectionsToolStripMenuItem.Size = new System.Drawing.Size(173, 22);
+            this.exportSelectionsToolStripMenuItem.Text = "Export Selections";
+            this.exportSelectionsToolStripMenuItem.Click += new System.EventHandler(this.exportSelectionsToolStripMenuItem_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.LineLabel});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 609);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(838, 22);
+            this.statusStrip1.TabIndex = 7;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // LineLabel
+            // 
+            this.LineLabel.Name = "LineLabel";
+            this.LineLabel.Size = new System.Drawing.Size(0, 17);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(838, 631);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.FilterBtn);
+            this.Controls.Add(this.FilterTextBox);
+            this.Controls.Add(this.LogBox);
+            this.Controls.Add(this.ReloadBtn);
+            this.Controls.Add(this.SelectFileBtn);
+            this.Controls.Add(this.PathTxtBox);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Log Filter";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResizeEnd += new System.EventHandler(this.Form1_ResizeEnd);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox PathTxtBox;
+        private System.Windows.Forms.ListBox LogBox;
+        private System.Windows.Forms.Button ReloadBtn;
+        private System.Windows.Forms.Button SelectFileBtn;
+        private System.Windows.Forms.TextBox FilterTextBox;
+        private System.Windows.Forms.Button FilterBtn;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openOriginalFileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveFilteredLogToolStripMenuItem;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel LineLabel;
+        private System.Windows.Forms.ToolStripMenuItem exportSelectionsToolStripMenuItem;
+    }
+}
+
---
+++ Log Filter/Form1.cs
@@ -0,0 +1,355 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Log_Filter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        List<string> FilterWords = new List<string>();
+        List<string> WatchLogQueue = new List<string>();
+
+        Thread WatchThread;
+        bool doWork = true;
+
+        StreamReader streamReader;
+
+        const long logLineLimit = 10000;
+        long TotalLinesProcessed = 0;
+        object locker = new object();
+
+        
+        bool TryFilterLogMsg(string msg)
+        {
+            TotalLinesProcessed++;
+
+            foreach (var filterword in FilterWords)
+            {
+                if (msg.IndexOf(filterword, StringComparison.OrdinalIgnoreCase) >= 0)
+                {
+                    string inputstr = TotalLinesProcessed.ToString("0000000") + "\t |  " + msg;
+
+                    WatchLogQueue.Add(inputstr);
+                    if (WatchLogQueue.Count > logLineLimit)
+                        WatchLogQueue.RemoveAt(0);
+
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        void BulkAddFilteredMsg(List<string> messages)
+        {
+            this.BeginInvoke(new MethodInvoker(delegate ()
+            {
+                lock (locker)
+                {
+                    LogBox.BeginUpdate();
+                    LogBox.Items.AddRange(messages.ToArray());
+                    GoToListBottom();
+                    LogBox.EndUpdate();
+                    WatchLogQueue.Clear();
+                }
+            }));
+        }
+
+        void HandleFileWatch()
+        {
+            // setup a file watch
+            FileSystemWatcher fileSystemWatcher = new FileSystemWatcher();
+            var wh = new AutoResetEvent(false);
+            DirectoryInfo dir = new DirectoryInfo(PathTxtBox.Text).Parent;
+            fileSystemWatcher.Path = dir.FullName;
+            fileSystemWatcher.EnableRaisingEvents = true;
+            fileSystemWatcher.Changed += (s, ev) => wh.Set();
+            // open a read only stream to tail it.
+
+            var msg = "";
+            while (doWork)
+            {
+                try
+                {
+                    if (streamReader == null)
+                    {
+                        doWork = false;
+                        break;
+                    }
+
+                    if (streamReader.BaseStream.CanRead) // is the file still open?
+                    {
+                        msg = streamReader.ReadLine(); // note: this was opened when we picked a file. we assume its valid here.
+                    }
+                    else
+                    {
+                        doWork = false;
+                        break;
+                    }
+                }
+                catch (Exception)
+                {
+                    msg = "<<<< UNABLE TO PARSE LINE >>>>";
+                }
+
+                if (msg != null)
+                {
+                    LogBox.Invoke(new MethodInvoker(delegate ()
+                    {
+                        lock (locker)
+                        {
+                            TryFilterLogMsg(msg);
+                        }
+
+                        Application.DoEvents();
+                    }));
+                }
+                else
+                {
+                    UpdateLineLabel();
+                    {
+                        // batch update now.
+                        if (WatchLogQueue.Count > 0)
+                        {
+                            this.BeginInvoke(new MethodInvoker(delegate ()
+                            {
+                                lock (locker)
+                                {
+                                    LogBox.BeginUpdate();
+                                    LogBox.Items.AddRange(WatchLogQueue.ToArray());
+
+                                    if (LogBox.Items.Count > logLineLimit)
+                                    {
+                                        long reduceby = LogBox.Items.Count - logLineLimit;
+                                        for (int i = 0; i < reduceby; i++)
+                                        {
+                                            LogBox.Items.RemoveAt(0);
+                                        }
+                                    }
+
+                                    GoToListBottom();
+                                    LogBox.EndUpdate();
+                                    WatchLogQueue.Clear();
+                                }
+                            }));
+                        }
+                    }
+                    wh.WaitOne(250);
+                }
+            }
+
+            fileSystemWatcher.EnableRaisingEvents = false; // shut it down first.
+            wh.Close();
+            WatchLogQueue.Clear();
+        }
+
+        void GoToListBottom()
+        {
+            int visibleItems = LogBox.ClientSize.Height / LogBox.ItemHeight;
+            LogBox.TopIndex = Math.Max(LogBox.Items.Count - visibleItems + 1, 0);
+        }
+
+        void UpdateLineLabel()
+        {
+            this.BeginInvoke(new MethodInvoker(delegate ()
+             {
+                 LineLabel.Text = "Displaying: " + LogBox.Items.Count.ToString() + " / " + TotalLinesProcessed.ToString();
+                 LineLabel.ForeColor = (LogBox.Items.Count == logLineLimit) ? Color.Red : Color.Black;
+             }));
+        }
+
+        ////////////////////////////////////////////////////////////
+
+        private void ReloadBtn_Click(object sender, EventArgs e)
+        {
+            // just reapply filters from file..
+            FilterBtn_Click(sender, e);
+        }
+
+        private void StartWatchThread()
+        {
+            WatchThread = new Thread(HandleFileWatch);
+            doWork = true;
+            WatchThread.Start();
+        }
+
+        private void StopWatchThread()
+        {
+            if (WatchThread != null && WatchThread.IsAlive)
+            {
+                doWork = false;
+                WatchThread.Join();
+            }
+        }
+
+        private void SelectFileBtn_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.DefaultExt = "log";
+            dlg.Filter = "log files (*.log)|*.log|All files (*.*)|*.*";
+            dlg.CheckFileExists = true;
+
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+                PathTxtBox.Text = dlg.FileName;
+                ReloadBtn_Click(sender, e);
+            }
+        }
+
+        private void FilterBtn_Click(object sender, EventArgs e)
+        {
+            LogBox.Items.Clear(); // clear display.
+            FilterWords.Clear(); // clear filter words.
+            TotalLinesProcessed = 0;
+
+            if (File.Exists(PathTxtBox.Text))
+            {
+                // stop any old watches...
+                StopWatchThread();
+
+                // clear anything pending.
+                WatchLogQueue.Clear();
+
+                // generate new filter list.
+                FilterWords = new List<string>(FilterTextBox.Text.Split('|'));
+                for (int i = 0; i < FilterWords.Count; i++)
+                {
+                    FilterWords[i] = FilterWords[i].Trim();
+                }
+
+                // reopen file.
+                if (streamReader != null)
+                    streamReader.Close();
+
+                // open new stream for reading and pump it all into the log data to parse.
+                streamReader = new StreamReader(new FileStream(PathTxtBox.Text, FileMode.Open, FileAccess.Read, FileShare.ReadWrite, 4096, FileOptions.Asynchronous));
+
+                while (!streamReader.EndOfStream)
+                {
+                    // add data to pending work.
+                    TryFilterLogMsg(streamReader.ReadLine());
+                }
+
+                // now continue to watch it for new lines.
+                StartWatchThread();
+            }
+
+           
+            // do a single insert for speed here.
+            LogBox.BeginUpdate();
+            LogBox.Items.AddRange(WatchLogQueue.ToArray());
+            WatchLogQueue.Clear();
+            GoToListBottom();
+            LogBox.EndUpdate();
+
+            UpdateLineLabel();
+        }
+
+        private void FilterTextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                FilterBtn_Click(sender, new EventArgs());
+            }
+        }
+
+        private void PathTxtBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                ReloadBtn_Click(sender, new EventArgs());
+            }
+        }
+
+        private void Form1_ResizeEnd(object sender, EventArgs e)
+        {
+            LogBox.Refresh();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            StopWatchThread();
+
+            if (streamReader != null)
+                streamReader.Close();
+        }
+
+        private void LogBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            // COPY all highlighted text
+            if (e.Control == true && e.KeyCode == Keys.C)
+            {
+                string temp = "";
+                foreach (object item in LogBox.SelectedItems) temp += item.ToString() + "\r\n";
+                Clipboard.SetText(temp);
+            }
+        }
+
+        private void openOriginalFileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (File.Exists(PathTxtBox.Text))
+            {
+                System.Diagnostics.Process.Start(PathTxtBox.Text);
+            }
+        }
+
+        private void saveFilteredLogToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog dlg = new SaveFileDialog();
+            dlg.DefaultExt = "log";
+            dlg.Filter = "log files (*.log)|*.log|All files (*.*)|*.*";
+
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+                string temp = "";
+                foreach (object item in LogBox.Items) temp += item.ToString() + "\r\n";
+                File.WriteAllText(dlg.FileName, temp);
+            }
+
+        }
+
+        private void exportSelectionsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog dlg = new SaveFileDialog();
+            dlg.DefaultExt = "log";
+            dlg.Filter = "log files (*.log)|*.log|All files (*.*)|*.*";
+
+            // write data in such a way that we consolidate all of the selected lines into 1 document, but preserve the idea that there are gaps between selections for readability.
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+                using (StreamWriter writer = new StreamWriter(dlg.FileName))
+                {
+                    int lastindex = 0;
+
+                    foreach (int itemIndex in LogBox.SelectedIndices)
+                    {
+
+                        object selection = LogBox.Items[itemIndex];
+
+                        if (itemIndex - lastindex > 1)
+                        {
+                            writer.WriteLine(" ");
+                            writer.WriteLine("...");
+                            writer.WriteLine(" ");
+                        }
+                        writer.WriteLine(selection.ToString());
+                        lastindex = itemIndex;
+                    }
+                }
+            }
+        }
+    }
+}
